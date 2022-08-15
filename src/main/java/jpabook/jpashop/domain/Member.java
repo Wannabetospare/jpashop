bd32ec56a0893c27c5c5b499d451fd753271b6af
Wannabetospare <qaz4430@naver.com>
2022-08-15 17:01:14 +0900

    h2 DB 연결, 테이블 생성까지 , 엔티티 설계시 주의점까지

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,7 +1,34 @@
 package jpabook.jpashop.domain;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address adrress;
+
+
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+
 
 
 
